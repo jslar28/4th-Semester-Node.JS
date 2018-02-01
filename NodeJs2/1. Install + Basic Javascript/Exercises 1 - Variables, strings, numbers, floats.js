ddea12e3ce7a0139afe412cd92c60a0c03609c72
Anders Latif <anders@github.com>
2018-02-01 15:21:20 +0100

    live coding for node js 2

--- NodeJs2/1. Install + Basic Javascript/Exercises 1 - Variables, strings, numbers, floats.js
+++ NodeJs2/1. Install + Basic Javascript/Exercises 1 - Variables, strings, numbers, floats.js
@@ -9,6 +9,10 @@ var lastName = "Latif";
 // show in the console
 // My first name is Anders and my last name is Latif
 
+var result = "My name is " + firstName + ". My last name is " + lastName;
+
+// console.log(result);
+
 //--------------------------------------
 //Exercise 2 - Numbers and Strings
 
@@ -18,6 +22,16 @@ var number = 1;
 // Add the year plus the number
 // The result should be 2018
 // You cannot touch line 1 or 2
+
+var result = parseInt(year) + number;
+
+var currentYear = Number(year) + number;
+
+var alsoCurrentYear = (+ year) + number;
+
+var testingTheTwoFuncs = "201a8";
+
+//console.log(Number(testingTheTwoFuncs));
 
 //--------------------------------------
 
@@ -30,15 +44,14 @@ var numberTwo = "2.30";
 // add those two numbers and show the result
 // you cannot touch line 1 neither line 2
 
+var sum = parseFloat(numberOne) + parseFloat(numberTwo);
+
+var sum = parseFloat("2.2") + parseFloat("1.1");
+
+//console.log(sum);
 
 //--------------------------------------
-//Exercise 4 - Add the numbers and the total with 2 decimals
-
-var numberOne = "1.10";
-var numberTwo = "2.30";
-
-//--------------------------------------
-//Exercise 5 - Decimals and average
+//Exercise 4 - Decimals and average
 
 var one = 10;
 var two = 45;
@@ -46,17 +59,31 @@ var three = 98;
 
 // Show in the console the avg. with 5 decimals
 
+var total = one + two + three;
+var avg = total / 3;
+
+// console.log(avg.toFixed(5))
+
 //--------------------------------------
-//Exercise 6 - Get the character by index
+//Exercise 5 - Get the character by index
 
 var letters = "abc"
 // Get me the character "c"
 
+var char = letters.charAt(2);
+var alsoChar = letters[2];
+
+//console.log(alsoChar);
+
 //--------------------------------------
-//Exercise 7 - Replace
+//Exercise 6 - Replace
 
 var fact = "You are learning javascript!";
 
 // capitalize the J in Javascript
+
+var facter = fact.replace("javascript", "Javascript");
+
+console.log(facter);
 
 //--------------------------------------
--- NodeJs2/1. Install + Basic Javascript/Exercises 2 - Objects.js
+++ NodeJs2/1. Install + Basic Javascript/Exercises 2 - Objects.js
@@ -7,10 +7,16 @@ var myObj = {"message": "Hello, earthling! I bring peace."};
 
 // Log the message 
 
+//console.log(myObj.message);
+
 //--------------------------------------
 //Exercise 2 - Defining an object. 
 
 // Create an object that has your name and age. 
+
+//var obj = {"name": "Søren", "age": 26};
+
+//console.log(obj);
 
 //--------------------------------------
 //Exercise 3 - Add a property 
@@ -19,11 +25,21 @@ var stackOverflow = {};
 
 // make a rule called isAllowed and let the value be true
 
+stackOverflow.isAllowed = true;
+
+console.log(stackOverflow);
+
 //--------------------------------------
 //Exercise 4 - Remove a property 
 
 var thisSong = {"description": "The best song in the world."}
 
 // remove the property "description" and add a property called "about" that should say "Just a tribute." 
+
+delete thisSong.description;
+
+thisSong.about = "Just a tribute."
+
+console.log(thisSong);
 
 //--------------------------------------
